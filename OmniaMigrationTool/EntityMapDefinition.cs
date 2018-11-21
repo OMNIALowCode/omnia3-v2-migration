25b0a1c070e69ceda7f812c7de4a74cfad78aa04
Guilherme Ferreira <guilherme.as.ferreira@gmail.com>
2018-11-21 12:51:46 +0000

    Add static code mapping to companies

---
+++ OmniaMigrationTool/EntityMapDefinition.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OmniaMigrationTool
+{
+    internal class EntityMapDefinition
+    {
+
+        public EntityMapDefinition(string sourceKind, string sourceCode, string targetKind, string targetCode, IList<AttributeMap> attributes)
+        : this(sourceKind, sourceCode, targetKind, targetCode, attributes, new List<EntityMapDefinition>())
+        {
+
+        }
+        public EntityMapDefinition(string sourceKind, string sourceCode, string targetKind, string targetCode, IList<AttributeMap> attributes, IList<EntityMapDefinition> items)
+        {
+            SourceKind = sourceKind;
+            SourceCode = sourceCode;
+            TargetKind = targetKind;
+            TargetCode = targetCode;
+            Attributes = attributes;
+            Items = items;
+        }
+
+        public string SourceKind { get; }
+        public string SourceCode { get; }
+
+        public string TargetKind { get; }
+        public string TargetCode { get; }
+        public IList<AttributeMap> Attributes { get; }
+
+        public IList<EntityMapDefinition> Items { get; }
+
+        internal class AttributeMap
+        {
+            public AttributeMap(string source, string target)
+            {
+                Source = source;
+                Target = target;
+            }
+
+            public string Source { get; }
+            public string Target { get; }
+        }
+    }
+
+
+}
--- OmniaMigrationTool/Program.cs
+++ OmniaMigrationTool/Program.cs
@@ -40,103 +40,197 @@ namespace OmniaMigrationTool
 
         private static async Task AsyncMain(string[] args)
         {
+            var sourceTenant = Guid.Parse("5b59faa8-3e4c-4d3e-82c8-2aecd1207a70");
+            var tempDir = new TempDirectory();
+
+            Console.WriteLine($"Writing to folder: {tempDir.Path}");
+
+            // EMPLOYEE 
+            // ---------------------------------------------
+            var employeeErpConfigDefinition = new EntityMapDefinition("MisEntityItem", "EmployeeERPConfig",
+                "GenericEntity", "EmployeeERPConfiguration",
+                "ERPCode,Vehicle,Department,DepartmentDescription,Job,JobDescription,Manager,Primavera,ExpensesManager".Split(",")
+                    .Select(c => new EntityMapDefinition.AttributeMap(c, c)).ToList());
+
+            employeeErpConfigDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("Code", "_code"));
+            employeeErpConfigDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("ERPCostCenter", "CostCenter"));
+            employeeErpConfigDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("CompanyCode", "company"));
+
+
+            var employeeDefinition = new EntityMapDefinition("Agent", "Employee",
+                "Agent", "Employee",
+                "Primavera,OutOfOffice,PrevYearHolidays,usesPreviousYearHolidays"
+                    .Split(",")
+                    .Select(c => new EntityMapDefinition.AttributeMap(c, c)).ToList(),
+                new List<EntityMapDefinition>()
+                {
+                    employeeErpConfigDefinition
+                });
+
+            employeeDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("Code", "_code"));
+            employeeDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("Name", "_name"));
+            employeeDefinition
+                .Attributes.Add(new EntityMapDefinition.AttributeMap("Company", "defaultCompany"));
+
+
+            // COMPANY 
+            // ---------------------------------------------
+            
+            var companyAttributes = new List<EntityMapDefinition.AttributeMap>()
+            {
+                new EntityMapDefinition.AttributeMap("Code", "_code"),
+                new EntityMapDefinition.AttributeMap("Name", "_name"),
+                new EntityMapDefinition.AttributeMap("BaseCurrency", "Currency"),
+                //new EntityMapDefinition.AttributeMap("ExpenseReportApprover", ""),
+                //new EntityMapDefinition.AttributeMap("CashAdvanceApprover", ""),
+                new EntityMapDefinition.AttributeMap("BaseLocation", "BaseLocation"),
+                //new EntityMapDefinition.AttributeMap("HRApprover", ""),
+                //new EntityMapDefinition.AttributeMap("NumberOfDays", ""),
+                //new EntityMapDefinition.AttributeMap("PrevYearVacationsLimitDay", ""),
+                //new EntityMapDefinition.AttributeMap("PrevYearVacationsLimitMonth", ""),
+                //new EntityMapDefinition.AttributeMap("SemesterContractVacationsLimitDay", ""),
+                //new EntityMapDefinition.AttributeMap("SemesterContractVacationsLimitMonth", ""),
+                //new EntityMapDefinition.AttributeMap("CanSeeHolidaysCreate", ""),
+                //new EntityMapDefinition.AttributeMap("Market", ""),
+                //new EntityMapDefinition.AttributeMap("Primavera", ""),
+                //new EntityMapDefinition.AttributeMap("HolidayEvents", ""),
+                //new EntityMapDefinition.AttributeMap("OvertimeEvents", ""),
+                //new EntityMapDefinition.AttributeMap("AbsenceEvents", "")
+            };
+
+            var companyDefinition = new EntityMapDefinition("Agent", "myCompany",
+                "Agent", "Company",
+                companyAttributes);
+
+
+            //// COMPANY CONFIGURATIONS
+            //// ---------------------------------------------
+
+            //var companyConfigAttributes = new List<EntityMapDefinition.AttributeMap>()
+            //{
+            //    new EntityMapDefinition.AttributeMap("Code", "_code"),
+            //    new EntityMapDefinition.AttributeMap("Name", "_name"),
+            //    new EntityMapDefinition.AttributeMap("Code", "Company"),
+            //    new EntityMapDefinition.AttributeMap("Primavera", "Primavera"),
+
+            //    new EntityMapDefinition.AttributeMap("BaseCurrency", "Currency"),
+            //    //new EntityMapDefinition.AttributeMap("ExpenseReportApprover", ""),
+            //    //new EntityMapDefinition.AttributeMap("CashAdvanceApprover", ""),
+            //    new EntityMapDefinition.AttributeMap("BaseLocation", "BaseLocation"),
+            //    //new EntityMapDefinition.AttributeMap("HRApprover", ""),
+            //    //new EntityMapDefinition.AttributeMap("NumberOfDays", ""),
+            //    //new EntityMapDefinition.AttributeMap("PrevYearVacationsLimitDay", ""),
+            //    //new EntityMapDefinition.AttributeMap("PrevYearVacationsLimitMonth", ""),
+            //    //new EntityMapDefinition.AttributeMap("SemesterContractVacationsLimitDay", ""),
+            //    //new EntityMapDefinition.AttributeMap("SemesterContractVacationsLimitMonth", ""),
+            //    //new EntityMapDefinition.AttributeMap("CanSeeHolidaysCreate", ""),
+            //    //new EntityMapDefinition.AttributeMap("Market", ""),
+                
+            //    //new EntityMapDefinition.AttributeMap("HolidayEvents", ""),
+            //    //new EntityMapDefinition.AttributeMap("OvertimeEvents", ""),
+            //    //new EntityMapDefinition.AttributeMap("AbsenceEvents", "")
+            //};
+
+            //var companyConfigDefinition = new EntityMapDefinition("Agent", "myCompany",
+            //    "GenericEntity", "CompanyConfigurations",
+            //    companyConfigAttributes);
+
+
+            stopwatch.Start();
+
             using (var conn = new SqlConnection("Data Source=sqlsrvmymis66ey4j7eutvtc.database.windows.net;Initial Catalog=sqldbmymis66ey4j7eutvtc;user id=MyMisMaster;password=4FXsJMDlp5JWHIzk;MultipleActiveResultSets=True;Connection Timeout=60"))
             {
                 await conn.OpenAsync();
 
-                var items = await GetItems(conn);
+                await ProcessEntity(tempDir.Path, sourceTenant, conn,
+                        employeeDefinition);
 
-                using (var command = new SqlCommand(@"SELECT * FROM [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities] me
-                    INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities_Agent] a on me.ID = a.ID
-                    LEFT JOIN (SELECT * FROM (
-                        SELECT av.MisEntityID, ak.Name, coalesce(foreignme.code, av.VALUE) AS Code
-                        FROM [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].AttributeKeys ak
-                        INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].MisEntityTypes t ON ak.MisEntityTypeID = t.ID
-                        INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].vwAttributeValues av ON ak.ID = av.AttributeKeyID
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].RelationalRules rr ON ak.ID = rr.PKID
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].RelationalRuleInstances rri on rr.ID = rri.RelationalRuleID and rri.PKID = av.id
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities] foreignme on rri.FKID = foreignme.ID
-                        WHERE t.Code = 'Employee'
-                        ) AS p
-                        PIVOT (MIN([Code]) FOR [Name] IN (Company, Primavera, OutOfOffice, PrevYearHolidays)) as pvt
-                    ) AS eav ON eav.MisEntityID = me.ID", conn))
+                await ProcessEntity(tempDir.Path, sourceTenant, conn,
+                    companyDefinition);
+            }
+
+            stopwatch.Stop();
+
+            Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+        }
+
+        private static async Task ProcessEntity(string outputPath, Guid sourceTenant, SqlConnection conn, EntityMapDefinition definition)
+        {
+            var itemDictionary = new Dictionary<string, List<ItemProcessed>>();
+
+            foreach(var item in definition.Items)
+                itemDictionary.Add(item.SourceCode, await GetItems(sourceTenant, conn, item));
+
+            using (var command = new SqlCommand(
+                Queries.SourceQueries.EntityQuery(sourceTenant,
+                    definition.SourceKind,
+                    definition.Attributes.Select(c => c.Source).ToArray()), conn))
+            {
+                command.Parameters.Add(new SqlParameter("@code", definition.SourceCode));
+
+
+                try
                 {
-                    //command.CommandType = CommandType.StoredProcedure;
-                    //command.Parameters.Add(new SqlParameter("@LastModifiedDate", modifiedLast));
 
-                    stopwatch.Start();
 
-                    try
+                    var sb = new StringBuilder();
+                    sb.AppendLine("identifier,version,body,created_at,updated_at");
+
+                    var jsonSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
+
+                    command.CommandTimeout = 360;
+
+                    using (var reader = await command.ExecuteReaderAsync())
                     {
-                        var tempDir = new TempDirectory();
-
-                        Console.WriteLine($"Writing to folder: {tempDir.Path}");
-
-                        var sb = new StringBuilder();
-                        sb.AppendLine("identifier,version,body,created_at,updated_at");
-
-                        var jsonSettings = new JsonSerializerSettings { ContractResolver = new CamelCasePropertyNamesContractResolver() };
-
-                        command.CommandTimeout = 360;
-
-                        using (var reader = await command.ExecuteReaderAsync())
+                        while (await reader.ReadAsync())
                         {
-                            while (await reader.ReadAsync())
+                            var sourceEntityId = reader.GetInt64(reader.GetOrdinal("ID"));
+                            var mapping = new Dictionary<string, object>();
+
+                            foreach (var attribute in definition.Attributes)
+                                MapAttribute(mapping, reader, attribute.Source, attribute.Target);
+
+                            foreach (var item in definition.Items)
                             {
-                                var sourceEntityId = reader.GetInt64(reader.GetOrdinal("ID"));
-                                var mapping = new Dictionary<string, object>
-                                {
-
-                                    {"usesPreviousYearHolidays", false},
-                                    {
-                                        "EmployeeERPConfiguration", items.Where(i=>i.ParentId.Equals(sourceEntityId)).Select(i=>i.Data)
-                                    }
-                                };
-
-                                MapAttribute(mapping, reader, "Code", "_code");
-                                MapAttribute(mapping, reader, "Name", "_name");
-                                MapAttribute(mapping, reader, "Company", "defaultCompany");
-
-                                sb.AppendLine($"{mapping["_code"]},1,\"{JsonConvert.SerializeObject(mapping, jsonSettings).Replace("\"", "\"\"")}\",{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss.ff")},{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss.ff")}");
+                                mapping[item.SourceCode] = itemDictionary[item.SourceCode].Where(i => i.ParentId.Equals(sourceEntityId))
+                                    .Select(i => i.Data);
                             }
+
+
+                            sb.AppendLine($"{mapping["_code"]},1,\"{JsonConvert.SerializeObject(mapping, jsonSettings).Replace("\"", "\"\"")}\",{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss.ff")},{DateTime.UtcNow.ToString("yyyy-MM-dd hh:mm:ss.ff")}");
                         }
-
-                        File.WriteAllText(Path.Combine(tempDir.Path, "employees.csv"), sb.ToString());
-                    }
-                    catch (Exception ex)
-                    {
-                        Console.WriteLine(ex.GetType().ToString());
-                        Console.WriteLine(ex.Message);
                     }
 
-                    stopwatch.Stop();
-
-                    Console.WriteLine("Time elapsed: {0}", stopwatch.Elapsed);
+                    File.WriteAllText(Path.Combine(outputPath, $"{definition.TargetCode}.csv"), sb.ToString());
                 }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.GetType().ToString());
+                    Console.WriteLine(ex.Message);
+                }
+
+
             }
         }
 
-        private static async Task<List<Item>> GetItems(SqlConnection conn)
+        private static async Task<List<ItemProcessed>> GetItems(Guid sourceTenant, SqlConnection conn,
+            EntityMapDefinition definition)
         {
-            var result = new List<Item>();
-            using (var command = new SqlCommand(@"SELECT * FROM [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities] me
-                    INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities_MisEntityItem] a on me.ID = a.ID
-                    LEFT JOIN (SELECT * FROM (
-                        SELECT av.MisEntityID, ak.Name, coalesce(foreignme.code, av.VALUE) AS Code
-                        FROM [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].AttributeKeys ak
-                        INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].MisEntityTypes t ON ak.MisEntityTypeID = t.ID
-                        INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].vwAttributeValues av ON ak.ID = av.AttributeKeyID 
-                        --INNER JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities] me ON me.ID = av.[Value]
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].RelationalRules rr ON ak.ID = rr.PKID
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].RelationalRuleInstances rri on rr.ID = rri.RelationalRuleID and rri.PKID = av.id
-                        LEFT JOIN [5b59faa8-3e4c-4d3e-82c8-2aecd1207a70].[MisEntities] foreignme on rri.FKID = foreignme.ID
-                        WHERE t.Code = 'EmployeeERPConfig'
-                        ) AS p
-                        PIVOT (MIN([Code]) FOR [Name] IN ([CompanyCode],[ERPCode],[Vehicle],[Department],[DepartmentDescription],[Job],[JobDescription],[ERPCostCenter],[Manager],[Primavera],[ExpensesManager])) as pvt
-                    ) AS eav ON eav.MisEntityID = me.ID", conn))
+            var result = new List<ItemProcessed>();
+            using (var command = new SqlCommand(
+                Queries.SourceQueries.EntityQuery(sourceTenant,
+                    definition.SourceKind,
+                    definition.Attributes.Select(c => c.Source).ToArray()
+                        ), conn))
             {
-                //command.CommandType = CommandType.StoredProcedure;
-                //command.Parameters.Add(new SqlParameter("@LastModifiedDate", modifiedLast));
+                command.Parameters.Add(new SqlParameter("@code",
+                    definition.SourceCode
+                ));
 
                 command.CommandTimeout = 360;
 
@@ -146,17 +240,11 @@ namespace OmniaMigrationTool
                     {
                         var mapping = new Dictionary<string, object>();
 
-                        MapAttribute(mapping, reader, "Code", "_code");
-                        MapAttribute(mapping, reader, "CompanyCode", "company");
-                        MapAttribute(mapping, reader, "ERPCode", "ERPCode");
-                        MapAttribute(mapping, reader, "Vehicle", "Vehicle");
-                        MapAttribute(mapping, reader, "ERPCostCenter", "CostCenter");
-                        MapAttribute(mapping, reader, "Manager", "Manager");
-                        MapAttribute(mapping, reader, "Primavera", "Primavera");
-                        MapAttribute(mapping, reader, "ExpensesManager", "ExpensesManager");
+                        foreach (var attribute in definition.Attributes)
+                            MapAttribute(mapping, reader, attribute.Source, attribute.Target);
 
                         var parentId = reader.GetInt64(reader.GetOrdinal("MisEntityID"));
-                        result.Add(new Item(parentId, mapping));
+                        result.Add(new ItemProcessed(parentId, mapping));
                     }
                 }
             }
@@ -172,9 +260,10 @@ namespace OmniaMigrationTool
             }
         }
 
-        internal class Item
+
+        internal class ItemProcessed
         {
-            public Item(long parentId, Dictionary<string, object> data)
+            public ItemProcessed(long parentId, Dictionary<string, object> data)
             {
                 ParentId = parentId;
                 Data = data;
