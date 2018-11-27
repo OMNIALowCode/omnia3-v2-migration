9f00d6207e4ac25b5257481cee75d3722c7bbfcc
Luis Barbosa <luis.barbosa@mymis365.onmicrosoft.com>
2018-11-27 16:26:11 +0000

    Changed Json Enum serialization

--- OmniaMigrationTool/EntityMapDefinition.cs
+++ OmniaMigrationTool/EntityMapDefinition.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -67,6 +69,7 @@ namespace OmniaMigrationTool
 
             public string SourceCardinality { get; }
 
+            [JsonConverter(typeof(StringEnumConverter))]
             public enum AttributeType
             {
                 Text,
